b131bee5b99f86d3870c5df700bedbb11e033a39
sebatoo <seleco23@gmail.com>
2024-02-19 16:02:16 +0100

    ProfileBar created

---
+++ serenadev2/serenadev2/Components/ProfileBar.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileBar.swift
+//  serenadev2
+//
+//  Created by Gustavo Sebastian Leon Cortez on 19/02/24.
+//
+
+import SwiftUI
+
+struct ProfileBar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProfileBar()
+}
