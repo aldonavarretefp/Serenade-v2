5935f0ea1871b1630cd408f1bebdb2d6a36d6ca2
Pablo Navarro Zepeda <panaze@outlook.com>
2024-02-26 17:18:08 +0100

    Changed the tab bar to a native tab bar

--- serenadev2/serenadev2/ContentView.swift
+++ serenadev2/serenadev2/ContentView.swift
@@ -14,27 +14,44 @@ struct ContentView: View {
     
     // MARK: - Body
     var body: some View {
-        VStack(spacing: 0){
-            // Switch to change between the tab that is selected
-            switch selectedTab {
-                
-            // Show the feed view
-            case .feed:
-                FeedView()
-                
-            // Show the search view
-            case .search:
-                SearchView()
-            
-            // Show the profile view
-            case .profile:
-                ProfileView()
-            }
-            
-            // MARK: - Custom tab bar
-            CustomTabBar(selectedTab: $selectedTab)
-                .zIndex(-1)
+        
+        TabView {
+            FeedView()
+                .tabItem {
+                    Image("feed.fill")
+                    Text("Feed")
+                }
+            SearchView()
+                .tabItem {
+                    Label("Search", systemImage: "magnifyingglass")
+                }
+            ProfileView()
+                .tabItem {
+                    Label("Profile", systemImage: "person.fill")
+                }
         }
+        
+//        VStack(spacing: 0){
+//            // Switch to change between the tab that is selected
+//            switch selectedTab {
+//                
+//            // Show the feed view
+//            case .feed:
+//                FeedView()
+//                
+//            // Show the search view
+//            case .search:
+//                SearchView()
+//            
+//            // Show the profile view
+//            case .profile:
+//                ProfileView()
+//            }
+//            
+//            // MARK: - Custom tab bar
+//            CustomTabBar(selectedTab: $selectedTab)
+//                .zIndex(-1)
+//        }
     }
 }
 
