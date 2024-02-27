563e3973d2a88572d970ca4b43a1918cce92abd9
RouinSPD <128640701+RouinSPD@users.noreply.github.com>
2024-02-27 21:26:51 +0100

    Updated background color of the view

--- serenadev2/serenadev2/Components/OpenWithView.swift
+++ serenadev2/serenadev2/Components/OpenWithView.swift
@@ -1,8 +1,6 @@
 //
 //  OpenWithView.swift
-//  serenadev2
-//
-//  Created by Pedro Daniel Rouin Salazar on 22/02/24.
+//  Created by Pedro Daniel Rouin Salazar on 21/02/24.
 //
 
 import SwiftUI
@@ -18,35 +16,40 @@ struct OpenWithView : View {
        
         NavigationStack {
             
-            VStack(alignment: .leading){
-                // A text view displaying instructions or information to the user.
-                Text("Listen to this song on the following music streaming platforms")
-                    .font(.title3) // Sets the font size and style.
-                    .fontWeight(.light) // Sets the weight of the font to light.
-                
-                // This is where the BrandsGrid view is placed, passing in the buttonTypes array.
-                // BrandsGrid is a custom view responsible for displaying the buttons.
-                BrandsGrid(buttonTypes: buttonTypes)
-                
-                // A spacer is used to push all preceding content to the top.
-                Spacer()
-            }
-            // Toolbar is used to add navigation bar items.
-            .toolbar{
-                // Toolbar item placed on the leading side of the navigation bar.
-                ToolbarItem(placement: .navigationBarLeading){
-                    Text("Open with")
-                        .font(.title2)
-                        .fontWeight(.semibold)
+            ZStack{
+                Rectangle()
+                    .fill(.ultraThickMaterial)
+                    .ignoresSafeArea()
+                VStack(alignment: .leading){
+                    // A text view displaying instructions or information to the user.
+                    Text("Listen to this song on the following music streaming platforms")
+                        .font(.title3) // Sets the font size and style.
+                        .fontWeight(.light) // Sets the weight of the font to light.
+                    
+                    // This is where the BrandsGrid view is placed, passing in the buttonTypes array.
+                    // BrandsGrid is a custom view responsible for displaying the buttons.
+                    BrandsGrid(buttonTypes: buttonTypes)
+                    
+                    // A spacer is used to push all preceding content to the top.
+                    Spacer()
                 }
-                // Toolbar item placed on the trailing side of the navigation bar
-                ToolbarItem(placement: .navigationBarTrailing){
-                    // An image view displaying a system icon.
-                    Image(systemName: "xmark.circle.fill")
+                // Toolbar is used to add navigation bar items.
+                .toolbar{
+                    // Toolbar item placed on the leading side of the navigation bar.
+                    ToolbarItem(placement: .navigationBarLeading){
+                        Text("Open with")
+                            .font(.title2)
+                            .fontWeight(.semibold)
+                    }
+                    // Toolbar item placed on the trailing side of the navigation bar
+                    ToolbarItem(placement: .navigationBarTrailing){
+                        // An image view displaying a system icon.
+                        Image(systemName: "xmark.circle.fill")
+                    }
                 }
+                // Adds horizontal padding to the VStack content.
+                .padding(.horizontal)
             }
-            // Adds horizontal padding to the VStack content.
-            .padding(.horizontal)
         }
     }
 }
@@ -54,3 +57,4 @@ struct OpenWithView : View {
 #Preview {
     OpenWithView(buttonTypes: [.appleMusic, .spotify, .youtubeMusic, .amazonMusic, .spotify])
 }
+
