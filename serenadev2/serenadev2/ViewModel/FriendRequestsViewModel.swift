0a87a8177bccd7cf08af1a201439dab9edc438f5
Aldo Navarrete <aldo.navarrete@fi.unam.edu>
2024-03-01 14:44:46 +0100

    added createFriendRequest, acceptFriendRequest, declineFriendRequest, acceptFriendRequestAndAddFriend, addFriend

--- serenadev2/serenadev2/ViewModel/FriendRequestsViewModel.swift
+++ serenadev2/serenadev2/ViewModel/FriendRequestsViewModel.swift
@@ -11,8 +11,10 @@ import CloudKit
 import Combine
 
 class FriendRequestsViewModel: ObservableObject {
+    
     @Published var friendRequests = [FriendRequest]()
     var cancellables = Set<AnyCancellable>()
+    
     /**
      Fetches the friend requests for the user.
      - Parameters:
@@ -33,4 +35,88 @@ class FriendRequestsViewModel: ObservableObject {
             .store(in: &cancellables)
         
     }
+
+    /**
+        Creates a friend request that links two users with their IDs.
+        - Parameters:
+            - senderID: The ID of the user that is sending the friend request.
+            - receiverID: The ID of the user that is receiving the friend request.
+    */
+    func createFriendRequest(senderID: CKRecord.ID, receiverID: CKRecord.ID) {
+        
+        let senderReference = CKRecord.Reference(recordID: senderID, action: .none)
+        let receiverReference = CKRecord.Reference(recordID: receiverID, action: .none)
+        
+        let friendRequest = FriendRequest(sender: senderReference, receiver: receiverReference, status: .pending, date: .now)
+        
+        CloudKitUtility.add(item: friendRequest) { result in
+            switch result {
+            case .success(_):
+                print("Success sending friend request!")
+                break;
+            case .failure(_):
+                break;
+            }
+        }
+    }
+
+    /**
+        Accepts a friend request from certain  and updates the status of the friend request to accepted.
+        - Parameters:
+            - friendRequest: The friend request to accept.
+    */
+    func acceptFriendRequest(friendRequest: FriendRequest) {
+        var updatedFriendRequest = friendRequest
+        updatedFriendRequest.status = .accepted
+        CloudKitUtility.update(item: updatedFriendRequest) { result in
+            switch result {
+            case .success(_):
+                print("Success accepting friend request!")
+                break;
+            case .failure(_):
+                break;
+            }
+        }
+    }
+
+    /**
+        Declines a friend request from certain  and updates the status of the friend request to declined.
+        - Parameters:
+            - friendRequest: The friend request to decline.
+    */
+    func declineFriendRequest(friendRequest: FriendRequest) {
+        var updatedFriendRequest = friendRequest
+        updatedFriendRequest.status = .rejected
+        CloudKitUtility.update(item: updatedFriendRequest) { result in
+            switch result {
+            case .success(_):
+                break;
+            case .failure(_):
+                break;
+            }
+        }
+    }
+
+    /**
+        Adds the sender to the receiver's friends list and viceversa.
+        - Parameters:
+            - friendRequest: The friend request to accept.
+    */
+    func acceptFriendRequestAndAddFriend(friendRequest: FriendRequest) {
+        let senderID = friendRequest.sender.recordID
+        let receiverID = friendRequest.receiver.recordID
+        let predicate = NSPredicate(format: "accountID == %@", senderID)
+        let userRecordType = UserRecordKeys.type.rawValue
+        print(senderID, receiverID, predicate, userRecordType)
+        
+    }
+    
+    private func addFriend(friend: User, to user: User) {
+        var updatedUser = user
+        let friendAccountID = friend.record.recordID
+        let reference = CKRecord.Reference(recordID: friendAccountID, action: .none)
+        updatedUser.friends?.append(reference)
+        CloudKitUtility.update(item: updatedUser) { _ in }
+    }
+
 }
