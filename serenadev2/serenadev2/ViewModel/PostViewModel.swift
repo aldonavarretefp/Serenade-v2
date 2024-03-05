55efc7ba8a3e00cd15982f337d0f067348910334
sebatoo <seleco23@gmail.com>
2024-03-05 10:55:15 +0100

    Created PostViewModel methods

--- serenadev2/serenadev2/ViewModel/PostViewModel.swift
+++ serenadev2/serenadev2/ViewModel/PostViewModel.swift
@@ -18,13 +18,46 @@ class PostViewModel: ObservableObject {
         
     }
     
-    func bringAllPosts(){
-        
+    func fetchAllPosts(user: User, completion: @escaping ([Post]?) -> Void){
+        let userID: CKRecord.Reference = CKRecord.Reference(recordID: user.record.recordID, action: .none)
+        if user.friends == nil {
+            //            print("NO FRIENDS")
+            fetchAllPostsFromUserID(id: user.record.recordID) { returnedPosts in
+                if returnedPosts != nil {
+                    completion(returnedPosts)
+                } else {
+                    completion(nil)
+                }
+            }
+        }
+        else {
+//                        print("FRIENDS: \(user.friends!)")
+            var userList = user.friends!
+            userList.append(userID)
+            let predicate = NSPredicate(format: "sender IN %@ && isActive == 1", userList)
+            let recordType = PostRecordKeys.type.rawValue
+            
+            CloudKitUtility.fetch(predicate: predicate, recordType: recordType)
+                .receive(on: DispatchQueue.main)
+                .sink { _ in
+                    
+                } receiveValue: { returnedPosts in
+                    let posts: [Post]? = returnedPosts
+                    guard posts != nil else {
+                        completion(nil)
+                        return
+                    }
+                    completion(posts)
+                }
+                .store(in: &cancellables)
+        }
     }
     
-    func bringAllPostFromUserID(id: CKRecord.Reference, completion: @escaping ([Post]?) -> Void){
-        let predicate = NSPredicate(format: "sender == %@", id)
-        let recordType = UserRecordKeys.type.rawValue
+    func fetchAllPostsFromUserID(id: CKRecord.ID, completion: @escaping ([Post]?) -> Void){
+        let recordToMatch = CKRecord.Reference(recordID: id, action: .none)
+        let predicate = NSPredicate(format: "sender == %@ && isActive == 1", recordToMatch)
+        //        print("FETCH POSTS PREDICATE: \(predicate)\n")
+        let recordType = PostRecordKeys.type.rawValue
         
         CloudKitUtility.fetch(predicate: predicate, recordType: recordType)
             .receive(on: DispatchQueue.main)
@@ -32,21 +65,19 @@ class PostViewModel: ObservableObject {
                 
             } receiveValue: { returnedPosts in
                 let posts: [Post]? = returnedPosts
-                guard let postss = posts else {
+                guard posts != nil else {
                     completion(nil)
                     return
                 }
-                completion(postss)
+                completion(posts)
             }
             .store(in: &cancellables)
     }
     
-    
     func createAPost(post: Post){
-        let newPost = Post(postType: post.postType, sender: post.sender, receiver: post.receiver, caption: post.caption, songId: post.songId, date: post.date, isAnonymous: post.isAnonymous, isActive: post.isActive)
+        let recordToMatch = CKRecord.Reference(recordID: post.sender!.recordID, action: .none)
+        let newPost = Post(postType: post.postType, sender: recordToMatch, receiver: post.receiver, caption: post.caption, songId: post.songId, date: post.date, isAnonymous: post.isAnonymous, isActive: post.isActive)
         CloudKitUtility.add(item: newPost) { _ in }
     }
-    
-    
 }
 
