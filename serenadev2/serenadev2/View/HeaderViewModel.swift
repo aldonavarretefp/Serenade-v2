9b046388e66f86bfd17da63887be3d4a3e09e1c0
Alejandro Oliva <alex_oliva1000@hotmail.com>
2024-03-16 20:17:04 +0100

    HeaderViewModel created

---
+++ serenadev2/serenadev2/View/HeaderViewModel.swift
@@ -0,0 +1,20 @@
+//
+//  HeaderViewModel.swift
+//  serenadev2
+//
+//  Created by Alejandro Oliva Ochoa on 16/03/24.
+//
+
+import Foundation
+
+final class HeaderViewModel: ObservableObject{
+    // Variables to hide/show the header
+    @Published var headerHeight: CGFloat = 0
+    @Published var headerOffset: CGFloat = 0
+    @Published var lastHeaderOffset: CGFloat = 0
+    @Published var direction: SwipeDirection = .none
+    @Published var shiftOffset: CGFloat = 0
+    
+    // Opacity variables for the button and the header
+    @Published var headerOpacity: Double = 1.0
+}
