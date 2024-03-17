1c0fb3e6e3a79756a86b50a28fdea86b360b70bf
Alejandro Oliva <alex_oliva1000@hotmail.com>
2024-03-17 19:49:50 +0100

    Added comments and removed unused things

--- serenadev2/serenadev2/View/SignInView.swift
+++ serenadev2/serenadev2/View/SignInView.swift
@@ -10,42 +10,50 @@ import AuthenticationServices
 
 struct SignInView: View {
     
+    // MARK: - ViewModel
+    @EnvironmentObject private var userViewModel: UserViewModel
+    
+    // MARK: - Environment properties
     @Environment(\.colorScheme) var colorScheme
     
-    @EnvironmentObject var userViewModel: UserViewModel
-    
+    // MARK: - Properties
     @State private var shouldShowUserDetails = false
     
+    // MARK: - Body
     var body: some View {
         NavigationStack {
             ZStack {
                 VStack {
+                    
+                    // Image of the app
                     Image("AppLogo")
                         .padding(.bottom)
                         .shadow(color: .black.opacity(0.15), radius: 13, x: 0, y: 8)
                     
+                    // Show a description so the user can know what this view is about
                     Text(LocalizedStringKey("StartSerenading"))
                         .bold()
                         .font(.largeTitle)
                         .multilineTextAlignment(.center)
                         .padding(.bottom)
                     
+                    // Display the sign in with apple button
                     SignInWithAppleButton(.signIn, onRequest: { request in
                         request.requestedScopes = [.email, .fullName]
                     }, onCompletion: { authResult in
                         switch authResult {
-                        case .success(let authResults):
+                        case .success(let authResults): // If the request is successfull
                             switch authResults.credential {
                             case let appleIDCredential as ASAuthorizationAppleIDCredential:
                                 let userID = appleIDCredential.user
                                 let fullName = [appleIDCredential.fullName?.givenName, appleIDCredential.fullName?.familyName].compactMap { $0 }.joined(separator: " ")
                                 let email = appleIDCredential.email ?? ""
-                                userViewModel.handleAuthorization(userID: userID, fullName: fullName, email: email)
+                                userViewModel.handleAuthorization(userID: userID, fullName: fullName, email: email) // Handle the authorization with the user info
                             default:
                                 break;
                             }
                         case .failure(let error):
-                            userViewModel.error = error.localizedDescription
+                            userViewModel.error = error.localizedDescription // Get the error
                             break;
                         }
                     })
@@ -54,27 +62,20 @@ struct SignInView: View {
                     .padding()
                     .padding(.top)
                     
-                    //                    if userViewModel.error != "" {
-                    //                        Text(userViewModel.error)
-                    //                    }
-                    
                 }
-                .signInWithAppleButtonStyle(colorScheme == .dark ? .white : .black)
                 .padding()
                 
             }
             .frame(maxWidth: .infinity, maxHeight: .infinity)
             .background(
                 ZStack{
+                    // Background of the view
                     LinearGradient(colors: [.accent, .clear], startPoint: .top, endPoint: .bottom)
                     Color.card.opacity(0.5)
                     
                 }
                     .ignoresSafeArea()
             )
-            .navigationBarTitleDisplayMode(.inline)
-            
-            
         }
         .transition(.asymmetric(insertion: .slide, removal: .push(from: .trailing)))
     }
