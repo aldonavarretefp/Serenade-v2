a3e7aaa3516a21ad6fbb54961ae702c7c4672429
Alejandro Oliva <alex_oliva1000@hotmail.com>
2024-03-17 20:52:26 +0100

    Added comments to the onboarding view and created the feature component

---
+++ serenadev2/serenadev2/Components/FeatureComponent.swift
@@ -0,0 +1,38 @@
+//
+//  FeatureComponent.swift
+//  serenadev2
+//
+//  Created by Alejandro Oliva Ochoa on 17/03/24.
+//
+
+import SwiftUI
+
+struct FeatureComponent: View {
+    var icon: String
+    var title: LocalizedStringKey
+    var description: LocalizedStringKey
+    
+    var body: some View {
+        HStack(alignment: .center, spacing: 32){
+            
+            Image(systemName: icon)
+                .font(.largeTitle)
+                .foregroundColor(.accentColor)
+                .padding(.leading)
+                .frame(maxWidth: 40)
+            
+            VStack(alignment: .leading, spacing: 5) {
+                Text(title)
+                    .fontWeight(.semibold)
+                    .font(.title2)
+                
+                Text(description)
+                    .foregroundColor(.callout)
+            }
+        }
+    }
+}
+
+#Preview {
+    FeatureComponent()
+}
--- serenadev2/serenadev2/View/OnboardingView.swift
+++ serenadev2/serenadev2/View/OnboardingView.swift
@@ -9,36 +9,37 @@ import SwiftUI
 
 struct OnboardingView: View {
     
-    //    ZStack{
-    //        LinearGradient(colors: [.accent, .clear], startPoint: .top, endPoint: .bottom)
-    //        Color.card.opacity(0.5)
-    //
-    //    }
-    //        .ignoresSafeArea()
-    
+    // MARK: - Properties
     @Binding var hasCompletedOnboarding: Bool
     
+    // MARK: - Body
     var body: some View {
         ZStack{
-            
+            // Background of the view
             Color.viewBackground
                 .ignoresSafeArea()
             
             VStack(){
+                // Welcome text
                 Text(LocalizedStringKey("WelcomeToSerenade"))
                     .font(.title)
                     .fontWeight(.bold)
                     .padding()
                 
+                // Scroll view if the content doesnt fit in the phone
                 ScrollView{
                     VStack(alignment: .leading, spacing: 40){
-                        FeatureView(icon: "person.fill.checkmark", title: LocalizedStringKey("AddFriends"), description: LocalizedStringKey("AddFriendsDescription"))
+                        // Add friends feature
+                        FeatureComponent(icon: "person.fill.checkmark", title: LocalizedStringKey("AddFriends"), description: LocalizedStringKey("AddFriendsDescription"))
                         
-                        FeatureView(icon: "waveform.circle.fill", title: LocalizedStringKey("DailySongSharing"), description: LocalizedStringKey("DailySharingDescription"))
+                        // Daily post feature
+                        FeatureComponent(icon: "waveform.circle.fill", title: LocalizedStringKey("DailySongSharing"), description: LocalizedStringKey("DailySharingDescription"))
                         
-                        FeatureView(icon: "play.circle.fill", title: LocalizedStringKey("ListenPreviews"), description: LocalizedStringKey("ListenPreviewsDescription"))
+                        // Listen previews feature
+                        FeatureComponent(icon: "play.circle.fill", title: LocalizedStringKey("ListenPreviews"), description: LocalizedStringKey("ListenPreviewsDescription"))
                         
-                        FeatureView(icon: "arrow.up.right.circle.fill", title: LocalizedStringKey("OpenWithFavorite"), description: LocalizedStringKey("OpenWithFavoriteDescription"))
+                        // Open with feature
+                        FeatureComponent(icon: "arrow.up.right.circle.fill", title: LocalizedStringKey("OpenWithFavorite"), description: LocalizedStringKey("OpenWithFavoriteDescription"))
                     }
                     .padding(.horizontal)
                 }
@@ -47,44 +48,14 @@ struct OnboardingView: View {
                 Spacer ()
                 ActionButton(label: LocalizedStringKey("Start"), symbolName: "", fontColor: .white, backgroundColor: .accentColor, isShareDaily: false, isDisabled: false) {
                     
-                    UserDefaults.standard.set(true, forKey: UserDefaultsKeys.hasCompletedOnboarding)
+                    UserDefaults.standard.set(true, forKey: UserDefaultsKeys.hasCompletedOnboarding) // Set to true so the user only see it once
                     hasCompletedOnboarding = true
-                    
                 }
                 .padding()
             }
-            
-        }
-        
-    }
-}
-
-struct FeatureView: View {
-    var icon: String
-    var title: LocalizedStringKey
-    var description: LocalizedStringKey
-    
-    var body: some View {
-        HStack(alignment: .center, spacing: 32){
-            
-            Image(systemName: icon)
-                .font(.largeTitle)
-                .foregroundColor(.accentColor)
-                .padding(.leading)
-                .frame(maxWidth: 40)
-            
-            VStack(alignment: .leading, spacing: 5) {
-                Text(title)
-                    .fontWeight(.semibold)
-                    .font(.title2)
-                
-                Text(description)
-                    .foregroundColor(.callout)
-            }
         }
     }
 }
-
 
 #Preview {
     OnboardingView(hasCompletedOnboarding: .constant(true))
