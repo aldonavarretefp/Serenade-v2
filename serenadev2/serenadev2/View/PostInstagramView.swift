e812596a902c0e56d68fffbc5e049b1e8ac4a5fa
Alejandro Oliva <alex_oliva1000@hotmail.com>
2024-03-18 13:16:25 +0100

    Added comments to the PostInstagramView

--- serenadev2/serenadev2/View/PostInstagramView.swift
+++ serenadev2/serenadev2/View/PostInstagramView.swift
@@ -9,32 +9,21 @@ import SwiftUI
 
 struct PostInstagramView: View {
     
+    // MARK: - Properties
     var sender: User?
     var post: Post?
     var song: SongModel?
     var artwork: Image
     var userImage: Image?
     
-    var formattedDate: Text {
-        let calendar = Calendar.current
-        
-        if calendar.isDateInToday(post!.date) {
-            return Text(LocalizedStringKey("Today"))
-        } else if calendar.isDateInYesterday(post!.date) {
-            return Text(LocalizedStringKey("Yesterday"))
-        } else {
-            let dateFormatter = DateFormatter()
-            dateFormatter.dateFormat = "MMM d, yyyy"
-            return Text(dateFormatter.string(from: post!.date))
-        }
-    }
-    
+    // MARK: - Body
     var body: some View {
-        
         VStack{
+            
             Spacer()
             
-            HStack(){
+            // Label with serenade and logo
+            HStack{
                 if let priColor = song?.priColor{
                     HStack{
                         Image(systemName: "waveform.circle.fill")
@@ -61,11 +50,13 @@ struct PostInstagramView: View {
             .opacity(0.9)
             .padding(.horizontal)
             
+            // Post
             VStack{
                 HStack{
                     if let sender = sender {
                         VStack(alignment: .leading){
                             HStack{
+                                // User profile picture
                                 if let userImage = userImage {
                                     userImage
                                         .resizable()
@@ -82,12 +73,14 @@ struct PostInstagramView: View {
                                         .frame(height: 28)
                                 }
                                 
+                                // Username
                                 Text("\(sender.tagName)")
                                     .bold()
                                 + Text("'s daily song")
                                     .foregroundStyle(.callout)
                             }
                             
+                            // Caption of the post
                             if let caption = post?.caption {
                                 if caption != "" {
                                     Text(caption)
@@ -102,7 +95,7 @@ struct PostInstagramView: View {
                 .padding()
                 
                 ZStack{
-                    
+                    // Artwork of the shared song in the back
                     artwork
                         .resizable()
                         .aspectRatio(contentMode: .fill)
@@ -111,18 +104,22 @@ struct PostInstagramView: View {
                         .clipShape(UnevenRoundedRectangle(cornerRadii: .init( bottomLeading: 15.0, bottomTrailing: 15.0)))
                     
                     HStack{
+                        // Artwork of the shared song in the front
                         artwork
                             .resizable()
                             .aspectRatio(contentMode: .fit)
                             .frame(width: 100, height: 100)
                             .clipShape(RoundedRectangle(cornerRadius: 10.0))
                         
+                        // Song info
                         VStack(alignment: .leading){
                             if let song = song {
+                                // Title of the song
                                 Text(song.title)
                                     .font(.title2)
                                     .bold()
                                 
+                                // Artist/s of the song
                                 Text(song.artists)
                                     .font(.title3)
                                     .foregroundStyle(.callout)
@@ -143,8 +140,6 @@ struct PostInstagramView: View {
             .background(.card)
             .clipShape(RoundedRectangle(cornerRadius: 15))
             .padding(.horizontal)
-            
-            
             
             Spacer()
         }
